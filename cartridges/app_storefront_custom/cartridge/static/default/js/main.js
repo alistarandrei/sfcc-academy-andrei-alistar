9d5453c7190bc07261206237827a0ece57ebb71c
Andrei Alistar <andrei.alistar@outlook.com>
2022-11-29 21:58:44 +0200

    Commit after implementing task using Bootstrap CSS classes

--- cartridges/app_storefront_custom/cartridge/static/default/js/main.js
+++ cartridges/app_storefront_custom/cartridge/static/default/js/main.js
@@ -2713,7 +2713,7 @@ module.exports = function (include) {
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: ENOENT: no such file or directory, open '/Users/andrei/Documents/GitHub/sfcc-academy-andrei-alistar/cartridges/app_storefront_custom/cartridge/client/default/js/components/clickCoupon.js'");
 
 /***/ }),
 
