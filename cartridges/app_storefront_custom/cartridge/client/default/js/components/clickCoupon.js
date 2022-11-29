751dfb8df820370bcc1e07ce6ab19cfe17153085
Andrei Alistar <andrei.alistar@outlook.com>
2022-11-29 20:51:17 +0200

    Commit after modifying cartPromoCode.isml and implementing logic in clickCoupon.js

---
+++ cartridges/app_storefront_custom/cartridge/client/default/js/components/clickCoupon.js
@@ -0,0 +1,9 @@
+$(".promo-code-form").hide();
+
+$(".coupon-code-click").on("click", function() {
+
+    $(".promo-code-form").show();
+
+    $(".coupon-code-click").hide();
+
+});
--- cartridges/app_storefront_custom/cartridge/static/default/js/main.js
+++ cartridges/app_storefront_custom/cartridge/static/default/js/main.js
@@ -2713,7 +2713,11 @@ module.exports = function (include) {
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-
+$(".promo-code-form").hide();
+$(".coupon-code-click").on("click", function () {
+  $(".promo-code-form").show();
+  $(".coupon-code-click").hide();
+});
 
 /***/ }),
 
